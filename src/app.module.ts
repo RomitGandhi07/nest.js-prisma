6dbbea2ba7429023e529c66ad33c332b1ca232b2
RomitGandhi07 <gandhiromit77@gmail.com>
2024-08-25 17:17:25 +0530

    Generated user, auth and bookmark modules

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, PrismaModule, BookmarkModule],
+})
+export class AppModule {}
